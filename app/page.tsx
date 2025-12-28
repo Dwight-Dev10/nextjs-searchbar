c79be55ef461d00ce5c88e6b0538a82cd4d31c4e
Dwight Hill <83855447+Dee-Dev1738@users.noreply.github.com>
2025-12-28 18:41:59 -0500

    features: add search component. Basic JSX and dummy data. No logic yet.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { React, useState } from "react";
+import TextField from "@mui/material/TextField";
+import List from "./Components/List";
+import "./App.css";
+
+function Home() {
   return (
     <div className="main">
       <h1>React Search</h1>
+      <div className="search">
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          fullWidth
+          label="Search"
+        />
+      </div>
+      <List />
     </div>
   );
 }
+
+export default Home;
